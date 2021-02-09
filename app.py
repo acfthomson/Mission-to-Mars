91d4e8f3d10d6d77d83000fd303b4d7593503499
Amanda Thomson <acfthomson@gmail.com>
2021-02-09 16:14:11 -0500

    module progress

--- app.py
+++ app.py
@@ -1,40 +1,24 @@
-# Import dependencies
-from flask import Flask, render_template
-from flask_pymongo import flask_pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import scraping
 
-# Set up Flask
 app = Flask(__name__)
 
-# Connect to Mongo using PyMongo
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-# Define route for HTML page
-@app.rout('/')
+@app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template('index.html', mars=mars)
+    return render_template("index.html", mars=mars)
 
-    # Set up scraping route
-    @app.route('/scrape')
-    def scrape():
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
 
-        # Assign a new variable that point to Mongo
-        mars = mongo.db.mars
-
-        # Create a variable that will hold newly scraped data
-        mars_data = scraping.scrape_all()
-
-        # Update the database
-        # {} adds an empty JSON object
-        # upsert=True tells Mongo to create a new document if one doesn't
-        # already exist
-        mars.update({}, mars_data, upsert=True)
-
-        # Navigate page back to / to see updated content
-        return redirect('/', code=302)
-
-        if __name__ == '__main__':
-            app.run()
-
+if __name__ == "__main__":
+   app.run()
--- old_scraping.py
+++ old_scraping.py
@@ -4,56 +4,61 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 import datetime as dt
 
-
+# Intialize the browser, create a data dictionary, end the WebDriver, and return
+# scraped data
 def scrape_all():
+
     # Initiate headless driver for deployment
     browser = Browser("chrome", executable_path="chromedriver", headless=True)
 
+    # Set news title and paragraph variables
     news_title, news_paragraph = mars_news(browser)
 
     # Run all scraping functions and store results in a dictionary
-    data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+    data={
+        'news_title': news_title,
+        'news_paragraph': news_paragraph,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(),
+        'last_modified': dt.datetime.now()
     }
 
     # Stop webdriver and return data
     browser.quit()
     return data
 
+def mars_news(broswer):
 
-def mars_news(browser):
-
-    # Scrape Mars News
-    # Visit the mars nasa news site
+    # Visit the Mars NASA news site
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
 
     # Optional delay for loading the page
-    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # This searches for elements with a specific combination of ul and li tags and item_list and slide atts
+    # This also tells the browser to wait 1 second before searching so that dynamic pages have time to load
+    browser.is_element_present_by_css('ul.item_list li.slide', wait_time=1)
 
-    # Convert the browser html to a soup object and then quit the browser
+    # Set up the HTML parser
     html = browser.html
     news_soup = soup(html, 'html.parser')
 
     # Add try/except for error handling
     try:
-        slide_elem = news_soup.select_one("ul.item_list li.slide")
-        # Use the parent element to find the first 'a' tag and save it as 'news_title'
-        news_title = slide_elem.find("div", class_="content_title").get_text()
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+        # Use the parent element to find the first 'a' tag and save it as a 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+    
         # Use the parent element to find the paragraph text
-        news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
 
     except AttributeError:
         return None, None
-
+    
     return news_title, news_p
 
-
 def featured_image(browser):
+
     # Visit URL
     url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
@@ -62,40 +67,50 @@ def featured_image(browser):
     full_image_elem = browser.find_by_tag('button')[1]
     full_image_elem.click()
 
-    # Parse the resulting html with soup
+    # Parse the resulting HTML with Soup
     html = browser.html
     img_soup = soup(html, 'html.parser')
 
     # Add try/except for error handling
     try:
-        # Find the relative image url
+
+        # Find the relative image URL
         img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 
     except AttributeError:
         return None
 
-    # Use the base url to create an absolute url
+    # Use base URL to create absolute URL
     img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
-
+    
     return img_url
 
+
 def mars_facts():
+
     # Add try/except for error handling
     try:
-        # Use 'read_html' to scrape the facts table into a dataframe
+
+
+        # Scrape table with a DF
+        # Create a new DF from the HTML table. Index of 0 tells Pandas to pull only the first table it encounter
         df = pd.read_html('http://space-facts.com/mars/')[0]
 
     except BaseException:
         return None
 
-    # Assign columns and set index of dataframe
+    # Assign columns to the new DF
     df.columns=['Description', 'Mars']
+
+    # Convert 'Description' column into the index and inplace=True means the updated index will remain in place,
+    # without having to reassign the DF to a new variable
     df.set_index('Description', inplace=True)
+    df
 
-    # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    # Convert DF back into HTML-ready code
+    return df.to_html(classes='table table-striped')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    # If running as script, print scraped data
+    # If running as a script, print scraped data
     print(scrape_all())
